b8c7e33336b996bf2e6305bbfc09ecdb5639a094
dariussh77 <dariussh77@gmail.com>
2023-05-15 14:34:25 +0300

    hederis pataisytas, pridėtas Logout

--- src/components/P3_Orgamisms/Header.jsx
+++ src/components/P3_Orgamisms/Header.jsx
@@ -22,15 +22,27 @@ const HeaderCSS=styled.header`
     }
 `;
 const Header = () => {
-    const {currentUser}=useContext(UsersContext);
+    const {currentUser,loggedIn, setCurrentUser,setLoggedIn}=useContext(UsersContext);
+    const fLogout=()=>{
+        setCurrentUser(null);
+        setLoggedIn(false);
+    };
     return ( 
         <HeaderCSS>
             <img src="https://pbs.twimg.com/profile_images/435931025458593792/OXByHB12_400x400.jpeg" alt="Čia Aš" />
             {currentUser&&<h4>vartotojas:{currentUser.userName}</h4>}
-            <ul>
-                <li><NavLink to='/'>Login</NavLink></li>
-                <li><NavLink to='/register'>Register</NavLink></li>
-            </ul>
+            {
+                !loggedIn
+                    ?<ul>
+                        <li><NavLink to='/'>Login</NavLink></li>
+                        <li><NavLink to='/register'>Register</NavLink></li>
+                    </ul>
+                    :<ul>
+                        <li><NavLink to='/home'>Home</NavLink></li>
+                        <li><NavLink to='/add'>Add</NavLink></li>
+                        <li><NavLink onClick={()=>fLogout()} to='/'>Logout</NavLink></li>
+                    </ul>
+            }
         </HeaderCSS>
      );
 }
