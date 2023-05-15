87d708306e0a41dcc03ad48a98339674df5c6c3d
dariussh77 <dariussh77@gmail.com>
2023-05-15 10:07:45 +0300

    Sukurti visi reikalaujami Page failai

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import './App.css';
+import { Route,Routes } from 'react-router-dom';
+import Header from './components/P3_Orgamisms/Header';
+import Login from './components/P5_Pages/Login';
+
 
 const App=()=>{
   return (
     <>
+        <Header/>
+        <Routes>
+          <Route path='/' element={<Login/>}/>
+        </Routes>
 
     </>
   );
---
+++ src/components/P5_Pages/Login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+    return ( 
+        <main>
+            Login
+        </main>
+     );
+}
+ 
+export default Login;
